3a47d86032dcb74dbfef942cda3017813765b107
Raghav Sharma <raghavatri312@gmail.com>
2021-04-13 13:46:19 +0530

    1st Commit

--- app/src/main/java/com/example/govote/MainActivity.kt
+++ app/src/main/java/com/example/govote/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.govote
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import com.example.govote.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnAuth.setOnClickListener{
+            val intent = Intent(this,AddhaarVoterAuthenticator::class.java)
+            startActivity(intent)
+        }
+
     }
 }
